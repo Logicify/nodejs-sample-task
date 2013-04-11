5622285fd96fd31abd3686fd439b4cd3b6b3f5d4
Alexander Cherednichenko <lexaux@gmail.com>
2013-04-11 16:30:08 +0300

    Mounted public resources at /

--- server.js
+++ server.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function (req, res) {
-    res.send('Sample application (Node.js, mongodb, elastic search, ext.js)');
+app.configure(function () {
+    app.use(express.static(__dirname + '/public'));
 });
+
+app.get('/rest/*', function (req, res) {
+    res.send('Future REST api.');
+})
 
 app.listen(3000);
 console.log('Listening on port 3000');
