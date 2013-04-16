014d68961daf7c168a1e373b2d9ae58752e57b4d
vdidenko <vdidenko@bitbucket.org>
2013-04-16 15:34:10 +0300

    Add MongoDB test

--- tests/app-test.js
+++ tests/app-test.js
@@ -1,14 +1,18 @@
 var request = require('supertest');
 var app = require('../app.js');
 
-var obj={"Title": "Sample title", "Text": "Sample text", "Author": "Chris Colman", "Tags": ["a", "b"]};
+var obj = {
+    "Title": "Sample title",
+    "Text": "Sample text",
+    "Author": "Chris Colman",
+    "Tags": ["a", "b"]}
 describe('Server App testing', function () {
 
     it('#NewBook', function (done) {
         request(app)
             .post('/rest/newBook')
             .send(obj)
-            .expect(200, done) ;
+            .expect(200, done);
         console.info(done)
     });
     it('#Search', function (done) {
---
+++ tests/db-provider-test.js
@@ -0,0 +1,44 @@
+var BookProvider = require('../book/book-data-provider.js').BookProvider;
+var should = require("chai");
+var bookProvider = new BookProvider();
+
+var testBook = {
+    "Title": "Sample title",
+    "Text": "Sample text",
+    "Author": "Chris Colman",
+    "Tags": ["a", "b"]
+
+};
+describe("REST API test", function () {
+
+    before(function (done) {
+
+        this.timeout(100);
+        setTimeout(done, 50);
+    });
+
+    describe("#Find All Books", function () {
+
+        it('should be return all db record', function (done) {
+            bookProvider.findAll(function (error, data) {
+                if (error) {
+                    console.info(JSON.stringify({"error": error}))
+                } else {
+                    done();
+                }
+            });
+        });
+        it('should be save new record in DB', function (done) {
+
+            bookProvider.save(testBook, function (error, book) {
+                if (error) {
+                    console.log('Error saving book. Mongodb: ' + error);
+                } else {
+                    console.log(JSON.stringify(book));
+                    done();
+                }
+            })
+        });
+    });
+
+});
